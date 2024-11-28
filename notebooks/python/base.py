0613515aec12010c99c7c750c26a7bd3edc3aedf
Hepheir <hepheir@gmail.com>
2024-11-28 22:45:18 +0900

    chore: update base.py

--- notebooks/python/base.py
+++ notebooks/python/base.py
@@ -3,6 +3,7 @@ import os
 import pathlib
 import typing
 
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -13,9 +14,13 @@ sns.set_style('whitegrid')
 
 plt.rcParams['font.family'] = 'D2Coding'
 plt.rcParams['axes.unicode_minus'] = False
-
+plt.rcParams["savefig.transparent"] = True
+plt.rcParams["savefig.bbox"] = 'tight'
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent.parent
+
+T_TICKS = typing.Union[typing.List[str], typing.List[int], typing.List[float]]
+T_LABELS = typing.List[str]
 
 
 def get_files(glob_pattern: str) -> typing.List[str]:
@@ -37,3 +42,16 @@ def get_dataframe(file: str) -> pd.DataFrame:
 
 def get_merged_dataframe(files: typing.List[str]) -> pd.DataFrame:
     return pd.concat(map(get_dataframe, files), ignore_index=True)
+
+
+def formatter(x: typing.Union[int, float]) -> str:
+    if x.is_integer():
+        return f"{int(x):,d}"
+    return f"{x:,.2f}"
+
+
+def formatted_ticks_kwargs(ticks: typing.List[int]) -> typing.Dict[str, typing.Union[T_TICKS, T_LABELS]]:
+    return {
+        'ticks': ticks,
+        'labels': [formatter(x) for x in ticks],
+    }
